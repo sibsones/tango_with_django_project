9a55e80304d8e9095889945d3d068b84f2843a12
Luis Sibson <2260702s@student.gla.ac.uk>
2019-01-31 16:58:21 +0000

    chapter 6

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 """ Some Field Types:
 • CharField, a field for storing character data (e.g. strings). Specify max_length to provide a
@@ -22,6 +23,12 @@ class Category(models.Model):
     
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category,Page
 
 # Templates can be seen as scaffolding
 
@@ -8,7 +8,8 @@ from rango.models import Category
 
 def index(request):  # The view itself
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,'pages':page_list}
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -16,3 +17,17 @@ def about(request):
     context_dict = {'boldmessage': "Rango says this is the about page!"}
     
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request,'rango/category.html',context_dict)
